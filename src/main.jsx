5555514b0a5ad282588ac1036dc065cc5b59b53f
Manav <101001298+imanav10@users.noreply.github.com>
2024-05-17 12:24:29 +0000

    commitx91

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
+import Page from "./Page.jsx";
+import OA from "./OurApproach.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Page />
+    <OA />
   </React.StrictMode>,
 );
